c40c1cc9a68ff3ebb14521ee9a0ffcb0efc84336
mc <dyd97wns@naver.com>
2023-08-27 18:26:47 +0900

    pokemonfinder

---
+++ components/pokemon-grid.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { useState } from "react"
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { PokemonCard } from "./pokemon-card";
+
+interface PokeomonGridProps {
+    pokemonList: any;
+}
+
+export function PokemonGrid ({ pokemonList } : PokeomonGridProps ){ 
+    const [ searchText, setSearchText ] = useState("");
+
+    const seacchFilter = (pokemonList: any) => {
+        return pokemonList.filter(
+            (pokemon: any) => pokemon.name.toLowerCase().includes(searchText.toLowerCase())
+        )
+    }
+
+    const filteredPokemonList = seacchFilter(pokemonList);
+    
+    return (
+        <>
+            <div>
+                <h3 className="text-2xl py-6 text-center">Search For Your Pokemon!</h3>
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="pokemonName">Pokemon Name</Label>
+                    <Input 
+                        type="text" 
+                        value={searchText}
+                        autoComplete="off"
+                        id="pokemonName"
+                        placeholder="Charizard, Pikachu, etc."
+                        onChange={ (e) => setSearchText(e.target.value)}
+                    />
+                </div>
+                <h3 className="text-3xl pt-12 pb-6 text-center">Pokemon Collection</h3>
+            </div>
+
+            <div className="mb-32 grid text-center lg:md-0 lg:grid-cols-3 lg:text-left">
+                {filteredPokemonList.map((pokemon : any ) => {
+                    return (
+                        <PokemonCard name={pokemon.name} key={pokemon.name + "Card"}/>
+                    )
+                })}
+            </div>
+        </>
+    )
+}
